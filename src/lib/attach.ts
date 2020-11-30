eaa23d42dbe02d13978261f58199da1da633d2d2
Joshua Byrd <phocks@gmail.com>
2020-11-30 16:52:05 +1000

    Support falback js

--- src/lib/attach.ts
+++ src/lib/attach.ts
@@ -1,7 +1,20 @@
 // Function to load script into page
 export const attach = (url: string) => {
   const scriptTag = document.createElement('script');
-  if (url.includes('index_modern.js')) scriptTag.setAttribute('type', 'module');
+
+  // Adds support for addModernJS:true in @abcnews/aunty
+  // Here we assume a few things
+  if (url.includes('index_modern.js')) {
+    scriptTag.setAttribute('type', 'module');
+
+    const noModuleTag = document.createElement('script');
+    noModuleTag.setAttribute("nomodule", "");
+
+    const baseUrl  = url.replace("index_modern.js", "")
+    noModuleTag.setAttribute("src", `${baseUrl}index.js`);
+    document.head.appendChild(noModuleTag);
+  }
+
   scriptTag.setAttribute('src', url);
   document.head.appendChild(scriptTag);
 };
