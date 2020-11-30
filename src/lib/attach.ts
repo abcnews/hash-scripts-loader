bed2760f8e2d02a1bb4fb16af3d1dbceafc4605b
Joshua Byrd <phocks@gmail.com>
2020-11-30 15:09:44 +1000

    (slightly hacky way of) handling modern js bundles as exported by aunty

--- src/lib/attach.ts
+++ src/lib/attach.ts
@@ -1,6 +1,7 @@
 // Function to load script into page
 export const attach = (url: string) => {
   const scriptTag = document.createElement('script');
+  if (url.includes('index_modern.js')) scriptTag.setAttribute('type', 'module');
   scriptTag.setAttribute('src', url);
   document.head.appendChild(scriptTag);
 };
